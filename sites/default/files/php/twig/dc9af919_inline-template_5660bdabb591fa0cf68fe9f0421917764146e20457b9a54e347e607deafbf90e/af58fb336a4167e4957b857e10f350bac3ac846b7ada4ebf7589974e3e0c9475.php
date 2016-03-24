ff342b5f01422419b0115bd12cf2057221efafb3
David Rozas <david.rozas@gmail.com>
2016-03-24 14:23:59 +0000

    Reviewing

---
+++ sites/default/files/php/twig/dc9af919_inline-template_5660bdabb591fa0cf68fe9f0421917764146e20457b9a54e347e607deafbf90e/af58fb336a4167e4957b857e10f350bac3ac846b7ada4ebf7589974e3e0c9475.php
@@ -0,0 +1,63 @@
+<?php
+
+/* {# inline_template_start #}{{ field_profile_first_name }} {{ field_profile_last_name }} */
+class __TwigTemplate_757a2d0f09ef7e0f37e06277388f1a4d074916cb62587e2623de3c903334ab93 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["field_profile_first_name"]) ? $context["field_profile_first_name"] : null), "html", null, true));
+        echo " ";
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, (isset($context["field_profile_last_name"]) ? $context["field_profile_last_name"] : null), "html", null, true));
+    }
+
+    public function getTemplateName()
+    {
+        return "{# inline_template_start #}{{ field_profile_first_name }} {{ field_profile_last_name }}";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+}
+/* {# inline_template_start #}{{ field_profile_first_name }} {{ field_profile_last_name }}*/
